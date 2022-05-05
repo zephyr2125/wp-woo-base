351fac3fb20ac6870d5ddbdadffb1b5b2226c5e2
Zephyr <h21.zephyr@gmail.com>
2022-05-05 10:27:35 +0700

    update account code

--- src/components/Accounts/orders/index.js
+++ src/components/Accounts/orders/index.js
@@ -1,11 +1,9 @@
 import React from "react";
 
-import Accounts from "../../components/Accounts";
-
 const Index = () => {
     return ( 
         <>
-            <Accounts />  
+            <h1>Orders</h1>
         </>
      );
 }
