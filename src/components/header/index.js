c8bd5724e0bd5bce8629256634801fc757e7f40c
Zephyr <h21.zephyr@gmail.com>
2022-05-11 13:47:13 +0700

    update seo.js, product.js, header.js

--- src/components/header/index.js
+++ src/components/header/index.js
@@ -1,12 +1,25 @@
 import React from "react";
 import { navigate, Link } from "gatsby";
-
+import { useQuery, gql } from "@apollo/client";
 import { getUser, logout } from "../../func/functions"
 
 import Minicart from "../Cart/minicart";
 
 const Header = () => {
     const USER = getUser();
+
+    const GET_HEADER = gql`
+        query MyQuery {
+            menuItems {
+            nodes {
+                label
+                url
+            }
+            }
+        }
+    `;
+
+    const { data } = useQuery(GET_HEADER);
 
     const LogoutHandler = () => {
         logout();
@@ -18,6 +31,17 @@ const Header = () => {
             <div className="header_wrapper">
                 <div aria-hidden="true" className="header_logo" onClick={() => { navigate("/") }}>
                     <div>Woo</div>
+                </div>
+                <div className="header_menu">
+                    <ul>
+                        {data &&
+                            data.menuItems.nodes.map((item) => (
+                                <li key={item.label}>
+                                    <Link to={item.url}>{item.label}</Link>
+                                </li>
+                            ))
+                        }
+                    </ul>
                 </div>
             </div>
             {
